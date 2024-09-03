60c808e9556acdd99f9c1c294ffcd8fcbe63e4ff
RefaelNadav <rn3857604@gmail.com>
2024-09-03 17:16:51 +0300

    Add project files.

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class LinkedList
+    {
+        
+        private Node Head;
+
+        public LinkedList()
+        {
+            Head = null;
+        }
+
+        public LinkedList(int data)
+        {
+            Head = new Node(data);
+        }
+
+        //O(n)
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if (Head == null)
+            {
+                Head = newNode;
+                return;
+            }
+
+            Node curr = Head;
+
+            while (curr.getNext != null)
+            {
+                curr = curr.getNext();
+            }
+
+            curr.setNext(newNode);
+        }
+
+        public string Display()
+        {
+            if (Head == null)
+            {
+                return "List is empty";
+            }
+            Node curr = Head;
+            string values = "";
+            while (curr.getNext != null)
+            {
+                values += $" -< {curr.getValue().ToString()}";
+                curr = curr.getNext();
+            }
+            return values;
+        }
+    }
+}
---
+++ DataStructure/Node.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class Node
+    {
+        private int Value;
+        private Node? Next;
+
+        public int getValue()
+        {
+            return Value;
+        }
+
+        public Node getNext()
+        {
+            return Next;
+        }
+
+        public void setValue(int value)
+        {
+            Value = value;
+        }
+
+        public void setNext(Node next)
+        {
+            Next = next;
+        }
+
+        public Node(int data)
+        {
+            Value = data;
+            Next = null;
+        }
+    }
+}
